854471bd64cc734e319b3125848e31b1fdc950eb
magajisuleiman <magajisuleiman1203@gmail.com>
2023-08-09 13:00:45 +0100

    created the amenity class

---
+++ amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module Amenity class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Inherits from BaseModel
+    """
+    name = ""
